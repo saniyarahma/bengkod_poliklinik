4ccfa7fd56332cb30a307b8e0d90f842e7d2a12d
saniyarahma <saniyarahma97@gmail.com>
2023-10-27 10:21:30 +0700

    first commit

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php 
+$databaseHost = 'localhost';
+$databaseName = 'poliklinik';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
